a662acaec3eafa97acdc30cf7dff0432ad385fa1
brb <victor_bouzas@hotmail.com>
2020-12-24 11:20:59 +0000

    Add day 24 solution

--- day_24/solution.py
+++ day_24/solution.py
@@ -1,154 +1,103 @@
+from __future__ import annotations
+from typing import NamedTuple, List
+import re
 from timeit import default_timer as timer
 
-TEST_INPUT = '389125467'
-INPUT = '364289715'
+DIRECTIONS = {
+    'w':  (-1, +1, 0),
+    'nw': (0, +1, -1),
+    'ne': (+1, 0, -1),
+    'e':  (+1, -1, 0),
+    'se': (0, -1, +1),
+    'sw': (-1, 0, +1)
+}
 
 
-class Node:
-    def __init__(self, value, nxt=None):
-        self.value = value
-        self.next = nxt
+class Tile(NamedTuple):
+    x: int
+    y: int
+    z: int
+
+    def next_position(self, direction) -> Tile:
+        x_add, y_add, z_add = DIRECTIONS.get(direction)
+        x = self.x + x_add
+        y = self.y + y_add
+        z = self.z + z_add
+
+        return Tile(x, y, z)
 
 
-class CrabGame:
+class Grid:
 
-    def __init__(self, labeling: str):
-        cups = [int(n) for n in labeling]
-        self.min = min(cups)
-        self.nodes = {}
+    def __init__(self, initial_tile=Tile(0, 0, 0)):
+        self.black_tiles = set()
+        self.current = initial_tile
 
-        prev = None
-        for pos in range(len(cups)-1, -1, -1):
-            node = Node(value=cups[pos], nxt=None)
-            node.next = prev
-            self.nodes[cups[pos]] = node
-            prev = node
+    def flip(self, direction):
+        self.current = self.current.next_position(direction)
 
-        self.current = self.nodes[cups[0]]
-        self.nodes[cups[len(cups)-1]].next = self.nodes[cups[0]]
+    def change_color(self):
+        if self.current in self.black_tiles:
+            self.black_tiles.remove(self.current)
+        else:
+            self.black_tiles.add(self.current)
 
-        self.moves = 0
+    def flip_many(self, lines: str):
+        for line in lines:
+            actions = self._parse(line)
+            self.restart()
+            for action in actions:
+                self.flip(action)
+            self.change_color()
 
-    def move(self):
-        self.moves += 1
+    @staticmethod
+    def _parse(line: str) -> List[str]:
+        return re.findall(r'(e|se|sw|w|nw|ne)', line)
 
-        pickups = []
-        pickup = self.current.next
-        for _ in range(3):
-            pickups.append(pickup.value)
-            pickup = pickup.next
-        self.current.next = pickup
-        destination = self.select_next_destination(pickups=pickups)
-        dest_node = self.nodes[destination]
+    def restart(self, tile=Tile(0, 0, 0)):
+        self.current = tile
+        self.change_color()
 
-        left_before = dest_node.next
-        dest_node.next = self.nodes[pickups[0]]
-        self.nodes[pickups[0]].next = self.nodes[pickups[1]]
-        self.nodes[pickups[1]].next = self.nodes[pickups[2]]
-        self.nodes[pickups[2]].next = left_before
-        self.current = self.current.next
+    def simulate_day(self):
+        new_black = self.get_new_black()
+        to_black = self.get_white_to_flip()
 
-    def n_moves(self, n=10):
-        for _ in range(n):
-            self.move()
+        self.black_tiles = new_black | to_black
 
-    def select_next_destination(self, pickups):
-        destination = self.current.value - 1
-        while destination >= self.min:
-            if destination not in pickups:
-                return destination
+    def get_white_to_flip(self):
+        white_adjacent = {}
+        for tile in self.black_tiles:
+            for direction in DIRECTIONS:
+                possible_tile = tile.next_position(direction)
+                if possible_tile not in self.black_tiles:
+                    white_adjacent[possible_tile] = white_adjacent.get(possible_tile, 0) + 1
+        return {tile for tile in white_adjacent if white_adjacent[tile] == 2}
 
-            destination -= 1
+    def get_new_black(self):
+        to_flip = set()
+        for tile in self.black_tiles:
+            counter = 0
+            for direction in DIRECTIONS:
+                if tile.next_position(direction) in self.black_tiles:
+                    counter += 1
+            if counter in (1, 2):
+                to_flip.add(tile)
 
-        return max(x for x in self.nodes.keys() if x not in pickups)
-
-    @property
-    def result(self):
-        s = self.nodes[1]
-        node = s.next
-        r = ''
-        while node != s:
-            r += str(node.value)
-            node = node.next
-        return r
-
-
-class CrabGame2:
-
-    def __init__(self, labeling: str):
-        cups = [int(n) for n in labeling]
-        self.min = min(cups)
-        self.nodes = {}
-
-        higher = None
-        for pos in range(len(cups)-1, -1, -1):
-            node = Node(value=cups[pos], nxt=None)
-            node.next = higher
-            self.nodes[cups[pos]] = node
-            higher = node
-
-        for value in range(1_000_000, 9, -1):
-            node = Node(value=value, nxt=higher)
-            self.nodes[value] = node
-            higher = node
-
-        self.nodes[cups[-1]].next = self.nodes[10]
-        self.current = self.nodes[cups[0]]
-        self.moves = 0
-
-    def move(self):
-        self.moves += 1
-
-        pickups = []
-        pickup = self.current.next
-        for _ in range(3):
-            pickups.append(pickup.value)
-            pickup = pickup.next
-        self.current.next = pickup
-        destination = self.select_next_destination(pickups=pickups)
-        dest_node = self.nodes[destination]
-
-        left_before = dest_node.next
-        dest_node.next = self.nodes[pickups[0]]
-        self.nodes[pickups[0]].next = self.nodes[pickups[1]]
-        self.nodes[pickups[1]].next = self.nodes[pickups[2]]
-        self.nodes[pickups[2]].next = left_before
-        self.current = self.current.next
-
-    def n_moves(self, n=10):
-        for _ in range(n):
-            self.move()
-
-    def select_next_destination(self, pickups):
-        destination = self.current.value - 1
-        while destination >= self.min:
-            if destination not in pickups:
-                return destination
-
-            destination -= 1
-
-        return max(x for x in self.nodes.keys() if x not in pickups)
-
-    @property
-    def result(self):
-        s = self.nodes[1]
-        n1 = s.next.value
-        n2 = s.next.next.value
-        print(n1, n2)
-        return n1*n2
+        return to_flip
 
 
 if __name__ == '__main__':
     start = timer()
-    game = CrabGame(labeling=INPUT)
-    game.n_moves(100)
-    end = timer()
-    print('\n', game.result)
-    print('Elapsed time: ', end - start)
+    with open('input.txt', 'r') as f:
+        raw_data = f.read().splitlines()
 
-    start = timer()
-    game = CrabGame2(labeling=INPUT)
-    game.n_moves(10_000_000)
+    grid = Grid()
+    grid.flip_many(raw_data)
+
+    print(f'Day 0: {len(grid.black_tiles)}')
+    for day in range(1, 101, 1):
+        grid.simulate_day()
+        print(f'Day {day}: {len(grid.black_tiles)}')
+
     end = timer()
-    print('\n', game.result)
-    print('Elapsed time: ', end - start)
+    print(f'Elapsed time: {end - start}')
