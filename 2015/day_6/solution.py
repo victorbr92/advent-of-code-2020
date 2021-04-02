3b628dc0d40d24236122c926a4061c1f5a6beceb
brb <victor_bouzas@hotmail.com>
2021-04-02 12:58:59 +0100

    Add solution

--- 2015/day_6/solution.py
+++ 2015/day_6/solution.py
@@ -1,5 +1,6 @@
 from typing import List, NamedTuple
 import re
+import numpy as np
 
 with open('input.txt', 'r') as f:
     raw_data = f.read().splitlines()
@@ -8,70 +9,73 @@ with open('input.txt', 'r') as f:
 class Position(NamedTuple):
     x: int
     y: int
+    brightness: int = 0
 
 
 class Grid:
 
     def __init__(self):
-        self.lit = set()
+        self.lights = np.zeros((1_000, 1_000), dtype=bool)
 
     @property
     def lights_on(self):
-        return len(self.lit)
-
-    def turn_on(self, start: Position, end: Position):
-
-        for x in range(start.x, end.x + 1):
-            for y in range(start.y, end.y + 1):
-                self.lit.add(Position(x, y))
-
-    def turn_off(self, start: Position, end: Position):
-
-        for x in range(start.x, end.x + 1):
-            for y in range(start.y, end.y + 1):
-                if Position(x, y) in self.lit:
-                    self.lit.remove(Position(x, y))
-
-    def toggle(self, start: Position, end: Position):
-
-        for x in range(start.x, end.x + 1):
-            for y in range(start.y, end.y + 1):
-
-                if Position(x, y) in self.lit:
-                    self.lit.remove(Position(x, y))
-
-                elif Position(x, y) not in self.lit:
-                    self.lit.add(Position(x, y))
+        return self.lights.sum()
 
     def parse_instructions(self, instructions: List[str]):
         for instruction in instructions:
             positions = re.findall(r'\d+', instruction)
 
-            start = Position(x=int(positions[0]), y=int(positions[1]))
-            end = Position(x=int(positions[2]), y=int(positions[3]))
+            start = (int(positions[0]), int(positions[1]))
+            end = (int(positions[2]), int(positions[3]))
 
             if instruction.startswith('turn on'):
-                self.turn_on(start, end)
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] = True
 
             elif instruction.startswith('turn off'):
-                self.turn_off(start, end)
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] = False
 
             elif instruction.startswith('toggle'):
-                self.toggle(start, end)
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] = ~self.lights[start[0]:end[0]+1, start[1]:end[1]+1]
 
             else:
                 raise ValueError('Wrong instruction')
 
-            print(self.lights_on)
+
+class NewGrid:
+
+    def __init__(self):
+        self.lights = np.zeros((1_000, 1_000), dtype=int)
+
+    @property
+    def brightness(self):
+        return self.lights.sum()
+
+    def parse_instructions(self, instructions: List[str]):
+        for instruction in instructions:
+            positions = re.findall(r'\d+', instruction)
+
+            start = (int(positions[0]), int(positions[1]))
+            end = (int(positions[2]), int(positions[3]))
+
+            if instruction.startswith('turn on'):
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] += 1
+
+            elif instruction.startswith('turn off'):
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] -= 1
+                self.lights = self.lights.clip(min=0)
+
+            elif instruction.startswith('toggle'):
+                self.lights[start[0]:end[0]+1, start[1]:end[1]+1] += 2
+
+            else:
+                raise ValueError('Wrong instruction')
 
 
 if __name__ == '__main__':
-    # grid = Grid()
-    # grid.turn_on(start=Position(0, 0), end=Position(2, 2))
-    # grid.turn_off(start=Position(0, 0), end=Position(0, 0))
-    # grid.toggle(start=Position(0, 0), end=Position(2, 2))
-    # print(grid.lights_on)
-
     grid = Grid()
     grid.parse_instructions(instructions=raw_data)
     print(grid.lights_on)
+
+    grid = NewGrid()
+    grid.parse_instructions(instructions=raw_data)
+    print(grid.brightness)
